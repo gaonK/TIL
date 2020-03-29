5b0dca3b09a91a408d9622eee64cd7d1e3ab427f
Gaon Kim <gaon.kim.dev@gmail.com>
2020-03-30 00:04:24 +0900

    TDD by Example ch.4

--- TDD by Example/1/src/test/java/MoneyTest.java
+++ TDD by Example/1/src/test/java/MoneyTest.java
@@ -11,10 +11,8 @@ public class MoneyTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    Dollar product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
   }
 
   @Test
